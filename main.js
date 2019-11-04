e58c391dc8207674da29479e25422864db88546b
ChrisChV <xnpiochv@gmail.com>
2019-11-04 15:34:36 -0500

    init TDweb library

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+//var TdClient = require('tdweb');
+import TdClient from 'tdweb'
+
+let options = {
+    logVerbosityLevel: 1,
+    jsLogVerbosityLevel: 3,
+    mode: 'wasm', 
+    prefix: 'tdlib_test',
+    readOnly: false,
+    isBackground: false,
+    useDatabase: false,
+};
+
+var client = new TdClient(options);
+
 document.write('hola mundo');
 console.log("hola mundo");
